2dde10620e906f28791232d6207272b9da687a96
idhaes <Ilyan.DeHaes@UGent.be>
2025-11-21 00:26:44 +0100

    Aanpassing database / models.py
    Migration moet nog gebeuren!

--- app/config.py
+++ app/config.py
@@ -1,6 +1,4 @@
 class Config:
     SECRET_KEY = '372a7c3f5eb06245837e94c0b7361a0549edefb8c73eaf924a399f0af9b2e15b'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Primetals@db.dlklychiybmzxpqfadks.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Md1vHlFrGD3yTkGH@db.dlklychiybmzxpqfadks.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-#Bovenstaande code niet aanpassen!!!
